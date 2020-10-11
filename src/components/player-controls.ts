227fca0335aaaa197bb19f02c28540bdbb5d34cf
Lars Krieger <lars.krieger@gmail.com>
2020-10-11 12:46:34 +0200

    update PlayerControls now shows time lapsed time left

--- src/components/player-controls.ts
+++ src/components/player-controls.ts
@@ -18,31 +18,6 @@ export class PlayerControls extends LitElement {
         place-self: center stretch;
       }
 
-      div.controller {
-        display: flex;
-        justify-content: center;
-        flex-direction: column;
-      }
-
-      div.controls {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin-bottom: 1.6rem;
-      }
-
-      div.controls fa-icon {
-        font-size: 1.4rem;
-        margin: 0 1.6rem;
-        cursor: pointer;
-      }
-
-      div.controls fa-icon:nth-child(3) {
-        padding: .8rem 1.1rem;
-        border: 1px solid white;
-        border-radius: 100%;
-      }
-
       div.current-track {
         display: flex;
         align-self: center;
@@ -60,6 +35,47 @@ export class PlayerControls extends LitElement {
         cursor: pointer;
         color: var(--error);
       } 
+
+      div.controller {
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+      }
+
+      div.controls {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+
+      div.controls fa-icon {
+        font-size: 1.4rem;
+        margin: 0 1.6rem;
+        cursor: pointer;
+      }
+
+      div.controls fa-icon:nth-child(3) {
+        padding: .8rem 1.1rem;
+        border: 1px solid white;
+        border-radius: 100%;
+      }
+
+    
+      div.controller .timeline {
+        display: flex;
+        width: 50%;
+        align-items: center;
+      }
+
+      div.controller .timeline litify-slider {
+        margin: 1.6rem;
+        flex: 1 1 auto;
+      }
+      
+      div.controller .timeline litify-slider {
+        margin: 1.6rem;
+        flex: 1 1 auto;
+      }
 
       div.devices {
         display: flex;
@@ -95,7 +111,11 @@ export class PlayerControls extends LitElement {
             <fa-icon icon="fas fa-step-forward"></fa-icon>
             <fa-icon icon="fas fa-redo"></fa-icon>
           </div>
-          <litify-slider></litify-slider>
+          <div class="timeline">
+            <litify-paragraph>1:45</litify-paragraph>
+            <litify-slider></litify-slider>
+            <litify-paragraph>2:35</litify-paragraph>
+          </div>
         </div>
         <div class="devices">
           <fa-icon icon="fas fa-indent"></fa-icon>
