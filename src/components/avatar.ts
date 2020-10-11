1bda5fc28273fae46a1fe1537b2d7f3c4fa9a338
Lars Krieger <lars.krieger@gmail.com>
2020-10-11 08:28:36 +0200

    update Component litify-avata large size

--- src/components/avatar.ts
+++ src/components/avatar.ts
@@ -32,8 +32,8 @@ export class Avatar extends LitElement {
       }
 
       img.large {
-        height: 4.8rem;
-        width: 4.8rem;
+        height: 4rem;
+        width: 4rem;
       }
     `
   };
